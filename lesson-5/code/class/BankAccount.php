524f1aa0a9d0447fae96bd057e17f8f45349b642
Fábio Sala <fjcsala@gmail.com>
2021-03-29 12:45:23 -0300

    Lesson 5 - Exercice code ajust.

--- lesson-5/code/class/BankAccount.php
+++ lesson-5/code/class/BankAccount.php
@@ -1,113 +1,117 @@
 <?php
 
-class BankAccount {
-    public $number;
-    protected $type;
-    private $owner;
-    private $balance;
-    private $status;
+    class BankAccount {
+        public $number;
+        protected $type;
+        private $owner;
+        private $balance;
+        private $status;
 
-    public function openAccount($n) {
-        $this->number;
-    }
+        public function openAccount($n, $t, $o) {
+            $this->number = $n;
+            $this->type = $t;
+            $this->owner = $o;
 
-    public function closeAccount() {
-        if($this->balance == 0 && $this->status == 1) {
+            if ($t == "cc") {
+                $this->balance = 50;
+                echo("Conta corrente criada com sucesso!</br>");
+            } else {
+                $this->balance = 150;
+                echo("Conta poupança criada com sucesso!</br>");
+            }
+
+            $this->status = 1;
+        }
+
+        public function closeAccount() {
+            if($this->balance == 0 && $this->status == 1) {
+                $this->status = 0;
+                echo("Sua conta foi encerrada com sucesso!</br>");
+            } else {
+                echo("Ocorreu um erro ao encerrar a sua conta!</br>");
+            }
+        }
+
+        public function depositAccount($v) {
+            if($this->status == 1) {
+                $this->balance = ($this->balance + $v);
+                echo("Depósito efetuado com sucesso!</br>");
+            } else {
+                echo("Ocorreu um erro ao efetuar o depósito!</br>");
+            }
+        }
+
+        public function withdrawAccount($v) {
+            if($this->status == 1 && $this->balance >= $v) {
+                $this->balance = ($this->balance - $v);
+                echo("Saque efetuado com sucesso!</br>");
+            } else {
+                echo("Ocorreu um erro ao efetuar o saque!</br>");
+            }
+        }
+
+        public function payTaxAccount() {
+            if($this->type == "cc") {
+                if ($this->status == 1 && $this->balance >= 12) {
+                    $this->balance = ($this->balance - 12);
+                    echo("Cobrança efetuada com sucesso!</br>");
+                } else {
+                    echo("Ocorreu um erro ao efetuar o pagamento da taxa!</br>");
+                }
+            } else {
+                if ($this->status == 1 && $this->balance >= 20) {
+                    $this->balance = ($this->balance - 20);
+                    echo("Cobrança efetuada com sucesso!</br>");
+                } else {
+                    echo("Ocorreu um erro ao efetuar o pagamento da taxa!</br>");
+                }
+            }
+        }
+
+        public function __construct() {
             $this->status = 0;
-            echo("Sua conta foi encerrada com sucesso!");
-        } else {
-            echo("Ocorreu um erro ao encerrar a sua conta!");
+            $this->balance = 0;
+        }
+
+        public function getNumber() {
+            return $this->number;
+        }
+
+        public function setNumber($n) {
+            return $this->number = $n;
+        }
+
+        public function getType() {
+            return $this->type;
+        }
+
+        public function setType($t) {
+            return $this->type = $t;
+        }
+
+        public function getOwner() {
+            return $this->owner;
+        }
+
+        public function setOwner($o) {
+            return $this->owner = $o;
+        }
+
+        public function getBalance() {
+            return $this->balance;
+        }
+
+        public function setBalance($b) {
+            return $this->balance = $b;
+        }
+
+        public function getStatus() {
+            return $this->status;
+        }
+
+        public function setStatus($s) {
+            return $this->status = $s;
         }
     }
-
-    public function depositAccount($v) {
-        if($this->status == 1) {
-            $this->balance = ($this->balance + $v);
-            echo("Depósito efetuado com sucesso!");
-        } else {
-            echo("Ocorreu um erro ao efetuar o depósito!");
-        }
-    }
-
-    public function withdrawAccount($v) {
-        if($this->status == 1 && $this->balance >= $v) {
-            $this->balance = ($this->balance - $v);
-            echo("Saque efetuado com sucesso!");
-        } else {
-            echo("Ocorreu um erro ao efetuar o saque!");
-        }
-    }
-
-    public function payTaxAccount() {
-        if($this->type == "cc") {
-            if ($this->status == 1 && $this->balance >= 12) {
-                $this->balance = ($this->balance - 12);
-                echo("Cobrança efetuada com sucesso!");
-            } else {
-                echo("Ocorreu um erro ao efetuar o pagamento da taxa!");
-            }
-        } else {
-            if ($this->status == 1 && $this->balance >= 20) {
-                $this->balance = ($this->balance - 20);
-                echo("Cobrança efetuada com sucesso!");
-            } else {
-                echo("Ocorreu um erro ao efetuar o pagamento da taxa!");
-            }
-        }
-    }
-
-    public function __construct($n, $t, $o) {
-        $this->number = $n;
-        $this->typer = $t;
-        $this->owner = $o;
-        $this->status = 1;
-
-        if ($t == "cc") {
-            $this->balance = 50;
-        } else {
-            $this->balance = 150;
-        }
-    }
-
-    public function getNumber() {
-
-    }
-
-    public function setNumber() {
-        
-    }
-
-    public function getType() {
-
-    }
-
-    public function setType() {
-        
-    }
-
-    public function getOwner() {
-
-    }
-
-    public function setOwner() {
-        
-    }
-
-    public function getBalance() {
-
-    }
-
-    public function setBalance() {
-        
-    }
-
-    public function getStatus() {
-
-    }
-
-    public function setStatus() {
-        
-    }
-}
 
 ?>
--- lesson-5/code/index.php
+++ lesson-5/code/index.php
@@ -1,9 +1,33 @@
 <?php
 
-require_once ('class/BankAccount.php');
+    require_once ('class/BankAccount.php');
 
-$p1 = new BankAccount("00001", "cp", "Fábio");
+    $p1 = new BankAccount();
+    $p1->openAccount("00001", "cp", "Jubileu");
+    $p1->depositAccount(300);
+    $p1->payTaxAccount();
+    echo("Conta: ".$p1->getNumber()."</br>");
+    if($p1->getType() == "cc") {
+        echo("Tipo: Conta Corrente </br>");
+    } else {
+        echo("Tipo: Conta Poupança </br>");
+    }
+    echo("Nome: ".$p1->getOwner()."</br>");
+    echo("Saldo: R$ ".$p1->getBalance()."</br>");
 
-print_r($p1);
+    echo("</p>");
+
+    $p2 = new BankAccount();
+    $p2->openAccount("00002", "cc", "Creusa");
+    $p2->depositAccount(500);
+    $p2->payTaxAccount();
+    echo("Conta: ".$p2->getNumber()."</br>");
+    if($p2->getType() == "cc") {
+        echo("Tipo: Conta Corrente </br>");
+    } else {
+        echo("Tipo: Conta Poupança </br>");
+    }
+    echo("Nome: ".$p2->getOwner()."</br>");
+    echo("Saldo: R$ ".$p2->getBalance()."</br>");
 
 ?>
